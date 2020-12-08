213f4cbef075fec94fc59883c60082921bf57ded
Andrey Nevinny <psoglav.ih8u@gmail.com>
2020-12-08 14:38:05 +0600

    refactored the code and beautified the directory

--- dataset_preparation.py
+++ dataset_preparation.py
@@ -3,23 +3,25 @@ import pickle
 import nltk
 
 lemmatizer = nltk.stem.WordNetLemmatizer()
-intents = json.loads(open('intents.json').read())
+intents = json.loads(open('datasets/intents.json').read())
 words = []
 classes = []
 documents = []
 ignore_letters = list('?.,!')
 
 for intent in intents['intents']:
-  for pattern in intent['patterns']:
-    word_list = nltk.word_tokenize(pattern)
-    words.extend(word_list)
-    documents.append((word_list, intent['tag']))
-    
-    if intent['tag'] not in classes:
-      classes.append(intent['tag'])
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
 
-words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(word)
+         for word in words if word not in ignore_letters]
+
 words = sorted(set(words))
 
-pickle.dump(words, open('dump\words.pkl', 'wb'))
-pickle.dump(classes, open('dump\classes.pkl', 'wb'))
+pickle.dump(words, open('dump/words.pkl', 'wb'))
+pickle.dump(classes, open('dump/classes.pkl', 'wb'))
